95150368cc4b0aaca78e1b1029c46c863c55f6b6
Kussemar <marcus.dofinsmurt@gmail.com>
2022-11-02 16:00:01 +0100

    Tilføjet StartMenu

---
+++ src/StartMenu.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class StartMenu {
+
+    public void menu(){
+
+        System.out.println("Welcome to TextFlix!");
+        System.out.println("Please Login or Register.");
+        System.out.println("If you want to Login type: Login\nIf you want to register type: Register");
+
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+
+        switch(choice.toLowerCase()){
+            case "login":
+            System.out.println("Type username");
+            scanner.nextLine();
+                System.out.println("Type password");
+                scanner.nextLine();
+
+                //TODO
+                /*
+                if(username&&password == correct){
+                    grant access lol
+                }
+                */
+                break;
+            case "register":
+                System.out.println("To register please type a username");
+                scanner.nextLine();
+                System.out.println("Please type a password");
+                scanner.nextLine();
+
+                //TODO
+                // Giv adgang til brugeren
+
+                break;
+
+            default:
+                System.out.println("Invalid number");
+                break;
+
+        }
+    }
+}
+
+
+
