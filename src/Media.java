a78d82d31971a571faa4bdc1c6255cec3fdad178
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2022-11-03 11:09:18 +0100

    Category og Media

---
+++ src/Media.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public abstract class Media {
+
+    int releaseYear;
+
+    String name;
+
+    //HOW TO ENUM?
+    ArrayList<Category> category;
+
+    double rating;
+
+public Media(int releaseYear, String name, ArrayList<Category> category, double rating){
+
+    this.releaseYear = releaseYear;
+    this.name = name;
+    this.category = category;
+    this.rating = rating;
+
+}
+
+
+    public abstract void watch() {
+        return;
+
+    }
+
+    public void watchLater(User user) {
+        return;
+
+    }
+
+    public void removeFromWatchLater(User user){
+        return;
+
+    }
+
+    public void addToPrevWatched(User user){
+        return;
+
+    }
+}
