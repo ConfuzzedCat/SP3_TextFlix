6a03bd82faedbe7586464fdf7109eaf1dfaca6a4
Kussemar <marcus.dofinsmurt@gmail.com>
2022-11-08 09:21:40 +0100

    Fixet souts i Main Menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -19,29 +19,29 @@ public class MainMenu implements Menu {
 
         ArrayList<String> list = new ArrayList<>(Arrays.asList("For user Management", "LibraryMenu", "Account", "Log out!"));
 
-        boolean tryAgain =true;
+        boolean tryAgain = true;
 
         while(tryAgain) {
             int choice = TextUI.getUserInput("Please choose one is the option:", list);
             switch (choice) {
                 case 1:
-                    System.out.println("For user Management");
+                    TextUI.sendMessage("For user Management");
                     tryAgain = false;
                     break;
                 case 2:
-                    System.out.println("LibraryMenu");
+                    TextUI.sendMessage("LibraryMenu");
                     tryAgain = false;
                     break;
                 case 3:
-                    System.out.println("Account");
+                    TextUI.sendMessage("Account");
                     tryAgain = false;
                     break;
                 case 4:
-                    System.out.println("Log out!");
+                    TextUI.sendMessage("Log out!");
                     tryAgain = false;
                     break;
                 default:
-                    System.out.println(" Invalid choice");
+                    TextUI.sendMessage("Invalid choice");
                     break;
 
             }
